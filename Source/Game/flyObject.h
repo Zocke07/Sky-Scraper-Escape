1628b35edce732bce96ce28bac857a7553f1563f
Zocke07 <rivan.wiratama@gmail.com>
2024-05-03 10:12:36 +0800

    Finish flyobject class?

---
+++ Source/Game/flyObject.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+#include "../Library/gameutil.h"
+
+namespace game_framework
+{
+    class flyObject : public CMovingBitmap
+    {
+    public:
+        flyObject(); // Constructor
+        void init();
+        void load();
+        bool isCollide();
+        bool isJumping();
+        void setCollide(bool flag);
+        void setJumping(bool flag);
+        void gravity();
+        void jump();
+        void getCurrentJump();
+    private:
+        bool jumping;
+        bool collide;
+        int currentJump;
+        int gravityConst = 4;
+        int maxJumpHeight = 0;
+        int jumpConst = 50;
+    };
+}

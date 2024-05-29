168145716c1455f9e7f4e883b8874e75c7b63ddd
Zocke07 <rivan.wiratama@gmail.com>
2024-05-29 22:14:04 +0800

    Fix character logic when pausing

--- Source/Game/levelInit.cpp
+++ Source/Game/levelInit.cpp
@@ -39,7 +39,7 @@ void levelInit::OnKeyDown(UINT nChar, UINT nRepCnt, UINT nFlags)
         }
     }
     
-    if (character.isCollide() == false && congrats == false)
+    if (character.isCollide() == false && congrats == false && pause == false)
     {
         character.OnKeyDown(nChar, nRepCnt, nFlags);
     }
